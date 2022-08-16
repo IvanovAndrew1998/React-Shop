013945f3a8de4844399b80e9190efba34769b187
IvanovAndrew1998 <ivanovanreyka@gmail.com>
2022-08-16 19:02:24 +0300

    Animation Kaef

--- src/Components/Header/HeaderFlexDown/FlexDown.jsx
+++ src/Components/Header/HeaderFlexDown/FlexDown.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GeoTag from './GeoTag'
+import styles from './Slider.module.css'
 
 const FlexDown = () => {
+    const [slided, setslided] = useState("")
+    const [rotated, setRotated] = useState("")
+
+    function slide() {
+        if (slided === "") {
+            setslided(styles.slided);
+            setRotated(styles.rotated);
+        }
+
+        else {
+            setslided('');
+            setRotated('');
+        }
+    }
+
     return (
         <div className="header-flexdown">
+
             <GeoTag />
 
             <div className="flexdown-right">
-                <nav>
-                    <ul>
+                <nav className={styles.navSlider}>
+                    <ul className={styles.slider + " " + slided}>
+                        <li>
+                            <a href="#">Кольца</a>
+                        </li>
+                        <li>
+                            <a href="#">Подвески</a>
+                        </li>
+                        <li>
+                            <a href="#">Серьги</a>
+                        </li>
+                        <li>
+                            <a href="#">Часы</a>
+                        </li>
+                        <li>
+                            <a href="#">Религия</a>
+                        </li>
+                        <li>
+                            <a href="#">Свадьба</a>
+                        </li>
+                        <li>
+                            <a href="#">Женщинам</a>
+                        </li>
+                        <li>
+                            <a href="#">Мужчинам</a>
+                        </li>
                         <li>
                             <a href="#">Кольца</a>
                         </li>
@@ -35,7 +76,7 @@ const FlexDown = () => {
                         </li>
                     </ul>
                 </nav>
-                <img className="arrow" src="src/arrow.svg" alt=""/>
+                <img className={"arrow" + " " + rotated + " " + styles.animation} src="src/arrow.svg" alt="" onClick={slide} />
             </div>
 
 
