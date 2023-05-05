e2c8cfa8916373cfa1adb10e01544c1d87c0e4f4
IvanovAndrew1998 <ivanovanreyka@gmail.com>
2023-05-05 19:17:49 +0400

    magnifier

--- src/Components/Pages/ProductPage/SectionOne/LeftPanel/LeftPanel.jsx
+++ src/Components/Pages/ProductPage/SectionOne/LeftPanel/LeftPanel.jsx
@@ -20,53 +20,7 @@ const LeftPanel = ({ images }) => {
 
   
 
-  // function imageZoom(imgId, resultId) {
-  //   let img, lens, result, cx, cy;
-  //   img = document.getElementById(imgId);
-  //   result = document.getElementById(resultId);
-
-  //   lens = document.createElement('DIV');
-  //   lens.setAttribute("class", "productImgZoomLens");
-
-  //   img.parentElement.insertBefore(lens, img);
-
-  //   cx = result.offsetWidth / lens.offsetWidth;
-  //   cy = result.offsetHeight / lens.offsetHeight;
-
-  //   result.style.backgroundImage = "url('" + img.src + "')";
-  //   result.style.backgroundSize = (img.width * cx) + 'px' + (img.height * cy) + 'px';
-
-  //   lens.addEventListener("mousemove", moveLens);
-  //   img.addEventListener("mousemove", moveLens);
-
-  //   lens.addEventListener("touchmove", moveLens);
-  //   img.addEventListener("touchmove", moveLens);
-
-  //   function moveLens(e) {
-  //     let pos, x, y;
-  //     pos = getCursorPos(e);
-  //     x = pos.x - (lens.offsetWidth / 2);
-  //     y = pos.y - (lens.offsetHeight / 2);
-  //     if (x > img.width - lens.offsetWidth) { x = (img.width - lens.offsetWidth); }
-  //     if (x < 0) { x = 0 ;}
-  //     if (y > img.height - lens.offsetHeight) { y = img.height - lens.offsetHeight; }
-  //     if (y < 0 ) { y = 0 ;}
-  //     lens.style.left = x + 'px';
-  //     lens.style.top = y + 'px';
-  //     result.style.backgroundPosition = "-" + (x * cx) + 'px -' + (y * cy) + 'px'; 
-  //   }
-  //   function getCursorPos(e) {
-  //     let a, x = 0, y = 0;
-  //     e = e || window.event;
-  //     a = img.getBoundingClientRect();
-  //     x = e.pageX - a.left;
-  //     y = e.pageY - a.top;
-  //     x = x - window.pageXOffset;
-  //     y = y - window.pageYOffset;
-  //     return { x : x , y : y };
-  //   }
-  // }
-  // imageZoom('productImage', 'productImgZoomed');
+ 
 
 
   return (
@@ -77,10 +31,14 @@ const LeftPanel = ({ images }) => {
         )}
         <a className="else" href="#">Еще {imageQuantity()}</a>
       </div>
-      <div id='parentImage' className="product-big">
+      <div id='parentImage' className="productImage">
         <GlassMagnifier
           imageSrc={currentImage}
+          magnifierBorderColor={'#FF8B66'}
+          magnifierBorderSize={1}
+          magnifierSize={'40%'}
 
+          
         />
         {/* <img id='productImage' className='productImage' src={currentImage} alt="" /> */}
 
